d59d081d239c4d9725bd7fb21bd64ece02c0a404
Alexandr <makspop7@gmail.com>
2025-05-07 13:00:02 +0300

    Test - 1.  students_and_mentor.py

--- 6.classes/students_and_mentor.py
+++ 6.classes/students_and_mentor.py
@@ -6,7 +6,7 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -21,15 +21,47 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        
+    def average_grade(self):
+        total_grades = 0
+        total_courses = 0
+        for grades in self.grades.values():
+            total_grades += sum(grades)
+            total_courses += len(grades)
+        return total_grades / total_courses if total_courses > 0 else 0
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_student(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+best_student = Student('Иван', 'Иванов', 'мужской')
 best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+
+cool_lecturer = Lecturer('Кирилл', 'Кириллов')
+cool_lecturer.courses_attached += ['Python']
+
+cool_reviewer = Reviewer('Александра', 'Александрова')
+cool_reviewer.courses_attached += ['Python']
+
+
+cool_lecturer.rate_hw(best_student, 'Python', 4)
+cool_lecturer.rate_hw(best_student, 'Python', 5)
+
+
+cool_reviewer.rate_student(best_student, 'Python', 3)
+
+print(best_student.grades) 
